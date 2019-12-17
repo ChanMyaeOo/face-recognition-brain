0659eec7cfc07f560d7719a52c9c262c68f85e80
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2019-12-17 23:03:10 +0630

    fixed security issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'tachyons';
-import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 
@@ -12,9 +11,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 
-const app = new Clarifai.App({
-  apiKey: 'f239e9a86ed749dd941a406e39e0c894'
-});
+
 
 const particlesOption = {
   particles: {
@@ -83,7 +80,13 @@ class App extends Component{
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl',{
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+              input: this.state.input
+          })
+        }).then(response => response.json())
     .then(response => {
       if(response){
         fetch('http://localhost:3000/image',{
