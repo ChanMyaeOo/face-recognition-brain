56f90c640cd8d2790d02448139a99906974c69a0
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2019-12-18 13:04:02 +0630

    changing localhost

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component{
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://guarded-everglades-19603.herokuapp.com/imageurl',{
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends Component{
         }).then(response => response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3000/image',{
+        fetch('https://guarded-everglades-19603.herokuapp.com/image',{
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -29,7 +29,7 @@ class Register extends React.Component{
     }
 
     onSubmitRegister = ()=>{
-        fetch('http://localhost:3000/register',{
+        fetch('https://guarded-everglades-19603.herokuapp.com/register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
